1827eef39211c558d699364d9a3de8ec019347c4
kjdeepak <kjdeepak4work@gmail.com>
2020-10-30 00:22:29 +0530

    Add and customize bootstrap, Add node-sass, Add required image asset for login screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Fast Fingers </h1>
+      <h1 className="btn btn-primary">Fast Fingers </h1>
     </div>
   );
 }
